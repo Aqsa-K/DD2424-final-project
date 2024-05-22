748b4dedcd19260d72ebf06e836296f379e6649b
Aqsa Kausar <aqsakausar30@gmail.com>
2024-05-23 00:09:34 +0500

    same as tobias settings

--- training.py
+++ training.py
@@ -29,20 +29,20 @@ WEIGHT_DECAY = 1e-4
 EPOCHS = 100
 
 # AUGMENTATION
-IMAGE_SIZE = 48  # We will resize input images to this size.
-PATCH_SIZE = 6  # Size of the patches to be extracted from the input images.
+IMAGE_SIZE = 32 #48  # We will resize input images to this size.
+PATCH_SIZE = 2 #6  # Size of the patches to be extracted from the input images.
 NUM_PATCHES = (IMAGE_SIZE // PATCH_SIZE) ** 2
 MASK_PROPORTION = 0.75  # We have found 75% masking to give us the best results.
 
 # ENCODER and DECODER
 LAYER_NORM_EPS = 1e-6
-ENC_PROJECTION_DIM = 128
-DEC_PROJECTION_DIM = 64
-ENC_NUM_HEADS = 4
-ENC_LAYERS = 6
-DEC_NUM_HEADS = 4
+ENC_PROJECTION_DIM = 192 #128
+DEC_PROJECTION_DIM =  192 #64
+ENC_NUM_HEADS = 3 # 4
+ENC_LAYERS = 12 #6
+DEC_NUM_HEADS = 3 #4
 DEC_LAYERS = (
-    8  # The decoder is lightweight but should be reasonably deep for reconstruction.
+    4  # The decoder is lightweight but should be reasonably deep for reconstruction.
 )
 ENC_TRANSFORMER_UNITS = [
     ENC_PROJECTION_DIM * 2,
