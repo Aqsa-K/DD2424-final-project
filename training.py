809ce5ea76b8346b99c881bbe936e3712085e34e
Peiheng Li KTH <145039330+LP-Tobias@users.noreply.github.com>
2024-05-23 00:59:52 +0200

    update

--- training.py
+++ training.py
@@ -1,7 +1,4 @@
 import os
-
-os.environ["KERAS_BACKEND"] = "tensorflow"
-
 import tensorflow as tf
 import keras
 from keras import layers
@@ -9,49 +6,10 @@ from keras import layers
 import matplotlib.pyplot as plt
 import numpy as np
 import random
+import json
 
-# Setting seeds for reproducibility.
-SEED = 42
-keras.utils.set_random_seed(SEED)
-
-# DATA
-BUFFER_SIZE = 1024
-BATCH_SIZE = 256
-AUTO = tf.data.AUTOTUNE
-INPUT_SHAPE = (32, 32, 3)
-NUM_CLASSES = 10
-
-# OPTIMIZER
-LEARNING_RATE = 5e-3
-WEIGHT_DECAY = 1e-4
-
-# PRETRAINING
-EPOCHS = 100
-
-# AUGMENTATION
-IMAGE_SIZE = 48  # We will resize input images to this size.
-PATCH_SIZE = 6  # Size of the patches to be extracted from the input images.
-NUM_PATCHES = (IMAGE_SIZE // PATCH_SIZE) ** 2
-MASK_PROPORTION = 0.75  # We have found 75% masking to give us the best results.
-
-# ENCODER and DECODER
-LAYER_NORM_EPS = 1e-6
-ENC_PROJECTION_DIM = 128
-DEC_PROJECTION_DIM = 64
-ENC_NUM_HEADS = 4
-ENC_LAYERS = 6
-DEC_NUM_HEADS = 4
-DEC_LAYERS = (
-    2  # The decoder is lightweight but should be reasonably deep for reconstruction.
-)
-ENC_TRANSFORMER_UNITS = [
-    ENC_PROJECTION_DIM * 2,
-    ENC_PROJECTION_DIM,
-]  # Size of the transformer layers.
-DEC_TRANSFORMER_UNITS = [
-    DEC_PROJECTION_DIM * 2,
-    DEC_PROJECTION_DIM,
-]
+from mae_model import *
+from google.cloud import storage
 
 (x_train, y_train), (x_test, y_test) = keras.datasets.cifar10.load_data()
 (x_train, y_train), (x_val, y_val) = (
@@ -72,342 +30,16 @@ test_ds = tf.data.Dataset.from_tensor_slices(x_test)
 test_ds = test_ds.batch(BATCH_SIZE).prefetch(AUTO)
 
 
-def get_train_augmentation_model():
-    model = keras.Sequential(
-        [
-            layers.Rescaling(1 / 255.0),
-            layers.Resizing(INPUT_SHAPE[0] + 20, INPUT_SHAPE[0] + 20),
-            layers.RandomCrop(IMAGE_SIZE, IMAGE_SIZE),
-            layers.RandomFlip("horizontal"),
-        ],
-        name="train_data_augmentation",
-    )
-    return model
-
-
-def get_test_augmentation_model():
-    model = keras.Sequential(
-        [
-            layers.Rescaling(1 / 255.0),
-            layers.Resizing(IMAGE_SIZE, IMAGE_SIZE),
-        ],
-        name="test_data_augmentation",
-    )
-    return model
-
-
-class Patches(layers.Layer):
-    def __init__(self, patch_size=PATCH_SIZE, **kwargs):
-        super().__init__(**kwargs)
-        self.patch_size = patch_size
-
-        # Assuming the image has three channels each patch would be
-        # of size (patch_size, patch_size, 3).
-        self.resize = layers.Reshape((-1, patch_size * patch_size * 3))
-
-    def call(self, images):
-        # Create patches from the input images
-        patches = tf.image.extract_patches(
-            images=images,
-            sizes=[1, self.patch_size, self.patch_size, 1],
-            strides=[1, self.patch_size, self.patch_size, 1],
-            rates=[1, 1, 1, 1],
-            padding="VALID",
-        )
-
-        # Reshape the patches to (batch, num_patches, patch_area) and return it.
-        patches = self.resize(patches)
-        return patches
-
-    def show_patched_image(self, images, patches):
-        # This is a utility function which accepts a batch of images and its
-        # corresponding patches and help visualize one image and its patches
-        # side by side.
-        idx = np.random.choice(patches.shape[0])
-        print(f"Index selected: {idx}.")
-
-        plt.figure(figsize=(4, 4))
-        plt.imshow(keras.utils.array_to_img(images[idx]))
-        plt.axis("off")
-        plt.show()
-
-        n = int(np.sqrt(patches.shape[1]))
-        plt.figure(figsize=(4, 4))
-        for i, patch in enumerate(patches[idx]):
-            ax = plt.subplot(n, n, i + 1)
-            patch_img = tf.reshape(patch, (self.patch_size, self.patch_size, 3))
-            plt.imshow(keras.utils.img_to_array(patch_img))
-            plt.axis("off")
-        plt.show()
-
-        # Return the index chosen to validate it outside the method.
-        return idx
-
-    # taken from https://stackoverflow.com/a/58082878/10319735
-    def reconstruct_from_patch(self, patch):
-        # This utility function takes patches from a *single* image and
-        # reconstructs it back into the image. This is useful for the train
-        # monitor callback.
-        num_patches = patch.shape[0]
-        n = int(np.sqrt(num_patches))
-        patch = tf.reshape(patch, (num_patches, self.patch_size, self.patch_size, 3))
-        rows = tf.split(patch, n, axis=0)
-        rows = [tf.concat(tf.unstack(x), axis=1) for x in rows]
-        reconstructed = tf.concat(rows, axis=0)
-        return reconstructed
-
-
-# Get a batch of images.
-image_batch = next(iter(train_ds))
-
-# Augment the images.
-augmentation_model = get_train_augmentation_model()
-augmented_images = augmentation_model(image_batch)
-
-# Define the patch layer.
-patch_layer = Patches()
-
-# Get the patches from the batched images.
-patches = patch_layer(images=augmented_images)
-
-# # Now pass the images and the corresponding patches
-# # to the `show_patched_image` method.
-# random_index = patch_layer.show_patched_image(images=augmented_images, patches=patches)
-
-# # Chose the same chose image and try reconstructing the patches
-# # into the original image.
-# image = patch_layer.reconstruct_from_patch(patches[random_index])
-# plt.imshow(image)
-# plt.axis("off")
-# plt.show()
-
-
-
-class PatchEncoder(layers.Layer):
-    def __init__(
-        self,
-        patch_size=PATCH_SIZE,
-        projection_dim=ENC_PROJECTION_DIM,
-        mask_proportion=MASK_PROPORTION,
-        downstream=False,
-        **kwargs,
-    ):
-        super().__init__(**kwargs)
-        self.patch_size = patch_size
-        self.projection_dim = projection_dim
-        self.mask_proportion = mask_proportion
-        self.downstream = downstream
-
-        # This is a trainable mask token initialized randomly from a normal
-        # distribution.
-        self.mask_token = tf.Variable(
-            tf.random.normal([1, patch_size * patch_size * 3]), trainable=True
-        )
-
-    def build(self, input_shape):
-        (_, self.num_patches, self.patch_area) = input_shape
-
-        # Create the projection layer for the patches.
-        self.projection = layers.Dense(units=self.projection_dim)
-
-        # Create the positional embedding layer.
-        self.position_embedding = layers.Embedding(
-            input_dim=self.num_patches, output_dim=self.projection_dim
-        )
-
-        # Number of patches that will be masked.
-        self.num_mask = int(self.mask_proportion * self.num_patches)
-
-    def call(self, patches):
-        # Get the positional embeddings.
-        batch_size = tf.shape(patches)[0]
-        positions = tf.range(start=0, limit=self.num_patches, delta=1)
-        pos_embeddings = self.position_embedding(positions[tf.newaxis, ...])
-        pos_embeddings = tf.tile(
-            pos_embeddings, [batch_size, 1, 1]
-        )  # (B, num_patches, projection_dim)
-
-        # Embed the patches.
-        patch_embeddings = (
-            self.projection(patches) + pos_embeddings
-        )  # (B, num_patches, projection_dim)
-
-        if self.downstream:
-            return patch_embeddings
-        else:
-            mask_indices, unmask_indices = self.get_random_indices(batch_size)
-            # The encoder input is the unmasked patch embeddings. Here we gather
-            # all the patches that should be unmasked.
-            unmasked_embeddings = tf.gather(
-                patch_embeddings, unmask_indices, axis=1, batch_dims=1
-            )  # (B, unmask_numbers, projection_dim)
-
-            # Get the unmasked and masked position embeddings. We will need them
-            # for the decoder.
-            unmasked_positions = tf.gather(
-                pos_embeddings, unmask_indices, axis=1, batch_dims=1
-            )  # (B, unmask_numbers, projection_dim)
-            masked_positions = tf.gather(
-                pos_embeddings, mask_indices, axis=1, batch_dims=1
-            )  # (B, mask_numbers, projection_dim)
-
-            # Repeat the mask token number of mask times.
-            # Mask tokens replace the masks of the image.
-            mask_tokens = tf.repeat(self.mask_token, repeats=self.num_mask, axis=0)
-            mask_tokens = tf.repeat(
-                mask_tokens[tf.newaxis, ...], repeats=batch_size, axis=0
-            )
-
-            # Get the masked embeddings for the tokens.
-            masked_embeddings = self.projection(mask_tokens) + masked_positions
-            return (
-                unmasked_embeddings,  # Input to the encoder.
-                masked_embeddings,  # First part of input to the decoder.
-                unmasked_positions,  # Added to the encoder outputs.
-                mask_indices,  # The indices that were masked.
-                unmask_indices,  # The indices that were unmaksed.
-            )
-
-    def get_random_indices(self, batch_size):
-        # Create random indices from a uniform distribution and then split
-        # it into mask and unmask indices.
-        rand_indices = tf.argsort(
-            tf.random.uniform(shape=(batch_size, self.num_patches)), axis=-1
-        )
-        mask_indices = rand_indices[:, : self.num_mask]
-        unmask_indices = rand_indices[:, self.num_mask :]
-        return mask_indices, unmask_indices
-
-    def generate_masked_image(self, patches, unmask_indices):
-        # Choose a random patch and it corresponding unmask index.
-        idx = np.random.choice(patches.shape[0])
-        patch = patches[idx]
-        unmask_index = unmask_indices[idx]
-
-        # Build a numpy array of same shape as patch.
-        new_patch = np.zeros_like(patch)
-
-        # Iterate of the new_patch and plug the unmasked patches.
-        count = 0
-        for i in range(unmask_index.shape[0]):
-            new_patch[unmask_index[i]] = patch[unmask_index[i]]
-        return new_patch, idx
-
-
-# Create the patch encoder layer.
-patch_encoder = PatchEncoder()
-
-# Get the embeddings and positions.
-(
-    unmasked_embeddings,
-    masked_embeddings,
-    unmasked_positions,
-    mask_indices,
-    unmask_indices,
-) = patch_encoder(patches=patches)
-
-
-# # Show a maksed patch image.
-# new_patch, random_index = patch_encoder.generate_masked_image(patches, unmask_indices)
-
-# plt.figure(figsize=(10, 10))
-# plt.subplot(1, 2, 1)
-# img = patch_layer.reconstruct_from_patch(new_patch)
-# plt.imshow(keras.utils.array_to_img(img))
-# plt.axis("off")
-# plt.title("Masked")
-# plt.subplot(1, 2, 2)
-# img = augmented_images[random_index]
-# plt.imshow(keras.utils.array_to_img(img))
-# plt.axis("off")
-# plt.title("Original")
-# plt.show()
-
-
-
-def mlp(x, dropout_rate, hidden_units):
-    for units in hidden_units:
-        x = layers.Dense(units, activation=tf.nn.gelu)(x)
-        x = layers.Dropout(dropout_rate)(x)
-    return x
-
-
-
-def create_encoder(num_heads=ENC_NUM_HEADS, num_layers=ENC_LAYERS):
-    inputs = layers.Input((None, ENC_PROJECTION_DIM))
-    x = inputs
-
-    for _ in range(num_layers):
-        # Layer normalization 1.
-        x1 = layers.LayerNormalization(epsilon=LAYER_NORM_EPS)(x)
-
-        # Create a multi-head attention layer.
-        attention_output = layers.MultiHeadAttention(
-            num_heads=num_heads, key_dim=ENC_PROJECTION_DIM, dropout=0.1
-        )(x1, x1)
-
-        # Skip connection 1.
-        x2 = layers.Add()([attention_output, x])
-
-        # Layer normalization 2.
-        x3 = layers.LayerNormalization(epsilon=LAYER_NORM_EPS)(x2)
-
-        # MLP.
-        x3 = mlp(x3, hidden_units=ENC_TRANSFORMER_UNITS, dropout_rate=0.1)
-
-        # Skip connection 2.
-        x = layers.Add()([x3, x2])
-
-    outputs = layers.LayerNormalization(epsilon=LAYER_NORM_EPS)(x)
-    return keras.Model(inputs, outputs, name="mae_encoder")
-
-
-
-def create_decoder(
-    num_layers=DEC_LAYERS, num_heads=DEC_NUM_HEADS, image_size=IMAGE_SIZE
-):
-    inputs = layers.Input((NUM_PATCHES, ENC_PROJECTION_DIM))
-    x = layers.Dense(DEC_PROJECTION_DIM)(inputs)
-
-    for _ in range(num_layers):
-        # Layer normalization 1.
-        x1 = layers.LayerNormalization(epsilon=LAYER_NORM_EPS)(x)
-
-        # Create a multi-head attention layer.
-        attention_output = layers.MultiHeadAttention(
-            num_heads=num_heads, key_dim=DEC_PROJECTION_DIM, dropout=0.1
-        )(x1, x1)
-
-        # Skip connection 1.
-        x2 = layers.Add()([attention_output, x])
-
-        # Layer normalization 2.
-        x3 = layers.LayerNormalization(epsilon=LAYER_NORM_EPS)(x2)
-
-        # MLP.
-        x3 = mlp(x3, hidden_units=DEC_TRANSFORMER_UNITS, dropout_rate=0.1)
-
-        # Skip connection 2.
-        x = layers.Add()([x3, x2])
-
-    x = layers.LayerNormalization(epsilon=LAYER_NORM_EPS)(x)
-    x = layers.Flatten()(x)
-    pre_final = layers.Dense(units=image_size * image_size * 3, activation="sigmoid")(x)
-    outputs = layers.Reshape((image_size, image_size, 3))(pre_final)
-
-    return keras.Model(inputs, outputs, name="mae_decoder")
-
-
 class MaskedAutoencoder(keras.Model):
     def __init__(
-        self,
-        train_augmentation_model,
-        test_augmentation_model,
-        patch_layer,
-        patch_encoder,
-        encoder,
-        decoder,
-        **kwargs,
+            self,
+            train_augmentation_model,
+            test_augmentation_model,
+            patch_layer,
+            patch_encoder,
+            encoder,
+            decoder,
+            **kwargs,
     ):
         super().__init__(**kwargs)
         self.train_augmentation_model = train_augmentation_model
@@ -416,6 +48,7 @@ class MaskedAutoencoder(keras.Model):
         self.patch_encoder = patch_encoder
         self.encoder = encoder
         self.decoder = decoder
+
     def calculate_loss(self, images, test=False):
         # Augment the input images.
         if test:
@@ -446,6 +79,7 @@ class MaskedAutoencoder(keras.Model):
         # total_loss = self.compute_loss(y=loss_patch, y_pred=loss_output)
         total_loss = self.compiled_loss(loss_patch, loss_output)
         return total_loss, loss_patch, loss_output
+
     def train_step(self, images):
         with tf.GradientTape() as tape:
             total_loss, loss_patch, loss_output = self.calculate_loss(images)
@@ -471,6 +105,7 @@ class MaskedAutoencoder(keras.Model):
         # return results
         self.compiled_metrics.update_state(loss_patch, loss_output)
         return {m.name: m.result() for m in self.metrics}
+
     def test_step(self, images):
         total_loss, loss_patch, loss_output = self.calculate_loss(images, test=True)
         # Update the trackers.
@@ -498,7 +133,6 @@ mae_model = MaskedAutoencoder(
     encoder=encoder,
     decoder=decoder,
 )
-
 
 # Taking a batch of test inputs to measure model's progress.
 test_images = next(iter(test_ds))
@@ -551,52 +185,6 @@ class TrainMonitor(keras.callbacks.Callback):
             # plt.close()
 
 
-
-# Some code is taken from:
-# https://www.kaggle.com/ashusma/training-rfcx-tensorflow-tpu-effnet-b2.
-
-
-class WarmUpCosine(keras.optimizers.schedules.LearningRateSchedule):
-    def __init__(
-        self, learning_rate_base, total_steps, warmup_learning_rate, warmup_steps
-    ):
-        super().__init__()
-
-        self.learning_rate_base = learning_rate_base
-        self.total_steps = total_steps
-        self.warmup_learning_rate = warmup_learning_rate
-        self.warmup_steps = warmup_steps
-        self.pi = tf.constant(np.pi)
-
-    def __call__(self, step):
-        if self.total_steps < self.warmup_steps:
-            raise ValueError("Total_steps must be larger or equal to warmup_steps.")
-
-        cos_annealed_lr = tf.cos(
-            self.pi
-            * (tf.cast(step, tf.float32) - self.warmup_steps)
-            / float(self.total_steps - self.warmup_steps)
-        )
-        learning_rate = 0.5 * self.learning_rate_base * (1 + cos_annealed_lr)
-
-        if self.warmup_steps > 0:
-            if self.learning_rate_base < self.warmup_learning_rate:
-                raise ValueError(
-                    "Learning_rate_base must be larger or equal to "
-                    "warmup_learning_rate."
-                )
-            slope = (
-                self.learning_rate_base - self.warmup_learning_rate
-            ) / self.warmup_steps
-            warmup_rate = slope * tf.cast(step, tf.float32) + self.warmup_learning_rate
-            learning_rate = tf.where(
-                step < self.warmup_steps, warmup_rate, learning_rate
-            )
-        return tf.where(
-            step > self.total_steps, 0.0, learning_rate, name="learning_rate"
-        )
-
-
 total_steps = int((len(x_train) / BATCH_SIZE) * EPOCHS)
 warmup_epoch_percentage = 0.15
 warmup_steps = int(total_steps * warmup_epoch_percentage)
@@ -615,7 +203,6 @@ lrs = [scheduled_lrs(step) for step in range(total_steps)]
 
 # Assemble the callbacks.
 train_callbacks = [TrainMonitor(epoch_interval=5)]
-
 
 optimizer = keras.optimizers.AdamW(
     learning_rate=scheduled_lrs, weight_decay=WEIGHT_DECAY
@@ -637,30 +224,16 @@ history = mae_model.fit(
 # print(f"Loss: {loss:.2f}")
 # print(f"MAE: {mae:.2f}")
 
-from google.cloud import storage
 
 def save_history_to_gcs(history_json):
-    # Set up the client
     client = storage.Client()
-    
-    # Specify your bucket name
     bucket_name = 'experiment_results_21_5_1818'
-    
-    # Create a bucket object
     bucket = client.bucket(bucket_name)
-    
-    # Create a blob object for the file
     blob = bucket.blob('history.json')
-    
-    # Upload the JSON string
     blob.upload_from_string(history_json, content_type='application/json')
-    
     print("History object saved to GCS")
 
 
-import json
-
-# Convert the history.history dict to JSON
 history_dict = history.history
 history_json = json.dumps(history_dict)
 save_history_to_gcs(history_json)
